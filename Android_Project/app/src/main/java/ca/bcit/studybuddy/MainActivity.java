6a9834307309771f6ca354d85e73fdcfdc64874f
eHAce <nickmcgrath@live.ca>
2020-01-27 19:18:23 -0800

    added parameters to Vancouver api call

--- Android_Project/app/src/main/java/ca/bcit/studybuddy/MainActivity.java
+++ Android_Project/app/src/main/java/ca/bcit/studybuddy/MainActivity.java
@@ -21,27 +21,31 @@ public class MainActivity extends AppCompatActivity {
 
 
         //This method is used for testing queries.
-        vancouverAPI();
+        vancouverAPI(49.2899, -123.0, 100000, 2);
     }
 
     /**
      * Currently a test method!
-     *
      * This method creates a query to Vancouver Data API.
+     *
+     * @param xCoord the x coordinate
+     * @param yCoord the y coordinate
+     * @param radius radius in meters
+     * @param rows the amount of entities to return
+     *
      */
-    public void vancouverAPI() {
+    public void vancouverAPI(double xCoord, double yCoord, int radius, int rows) {
         // Instantiate the RequestQueue.
         final TextView textView = (TextView) findViewById(R.id.TestText);
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://opendata.vancouver.ca/api/records/1.0/search//?dataset=libraries&lang=en&rows=20&geofilter.distance=49.2899%2C+-123%2C+1000000";
-
+        String url = "https://opendata.vancouver.ca/api/records/1.0/search//?dataset=libraries&lang=en&rows=" + rows + "&geofilter.distance=" + xCoord + "%2C+" + yCoord + "%2C+" + radius;
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
-                        textView.setText("Response is: " + response.substring(0, 500));
+                        textView.setText("Response is: " + response);
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -49,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText("That didn't work!");
             }
         });
-
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
     }
