c3ba095652ecdedcea473f823a0616f624565ef5
Yuni <yshuh0126@gmail.com>
2020-02-10 00:34:54 -0800

    Added hamburger menu and subitems, and dependency for NavigationView

--- Android_Project/app/src/main/java/ca/bcit/studybuddy/LandingActivity.java
+++ Android_Project/app/src/main/java/ca/bcit/studybuddy/LandingActivity.java
@@ -1,23 +1,49 @@
 package ca.bcit.studybuddy;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class LandingActivity extends AppCompatActivity {
 
     String[] librariesNearby = {"nearest", "second-nearest", "third-nearest"};
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ListView
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
                 R.layout.activity_listview, librariesNearby);
 
         ListView listView = findViewById(R.id.library_list);
         listView.setAdapter(adapter);
+
+        // Hamburger menu bar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
