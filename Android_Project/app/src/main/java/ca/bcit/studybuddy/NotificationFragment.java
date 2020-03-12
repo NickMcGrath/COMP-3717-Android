36a4dbcab9dc416e4911d48e40c27905c630944f
Yuni <yshuh0126@gmail.com>
2020-03-11 21:46:15 -0700

    Added notification fragment that contains list of other user's requests

--- Android_Project/app/src/main/java/ca/bcit/studybuddy/NotificationFragment.java
+++ Android_Project/app/src/main/java/ca/bcit/studybuddy/NotificationFragment.java
@@ -1,20 +1,82 @@
 package ca.bcit.studybuddy;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class NotificationFragment extends Fragment {
+import static com.facebook.FacebookSdk.getApplicationContext;
 
+public class NotificationFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+
+        final String names[] = {"Gina","Nick","Yuni"};
+        final String schools[] = {"BCIT","BCIT","BCIT"};
+        final int images[] = {R.drawable.logo, R.drawable.logo, R.drawable.logo};
+
+        ListView listView = (ListView) view.findViewById(R.id.requests_list);
+
+        MyAdapter adapter = new MyAdapter(getActivity(), names, schools, images);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == 0){
+                    Toast.makeText(getActivity(), "first request", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        return view;
     }
+
+    class MyAdapter extends ArrayAdapter<String>{
+        Context context;
+        String rNames[];
+        String rSchools[];
+        int rImages[];
+
+        MyAdapter(Context c, String name[], String school[],int img[]){
+            super(c, R.layout.fragment_notification_listview, R.id.textView1, name);
+            this.context = c;
+            this.rNames = name;
+            this.rSchools = school;
+            this.rImages = img;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.fragment_notification_listview, parent,false );
+            //View view = inflater.inflate(R.layout.fragment_notification, container, false);
+            ImageView imageView = row.findViewById(R.id.profile_image);
+            TextView aName = row.findViewById(R.id.textView1);
+            TextView aSchool = row.findViewById(R.id.textView2);
+
+            return row;
+        }
+    }
+
 }
