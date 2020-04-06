5d14f681c129c5aba254c4fdc6b29930e861c427
Yuni <yshuh0126@gmail.com>
2020-04-05 20:23:23 -0700

    Added a popup layout for the view profile button in NotificationFragment

--- Android_Project/app/src/main/java/ca/bcit/studybuddy/NotificationFragment.java
+++ Android_Project/app/src/main/java/ca/bcit/studybuddy/NotificationFragment.java
@@ -1,14 +1,19 @@
 package ca.bcit.studybuddy;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
+import android.media.TimedText;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -20,7 +25,7 @@ import androidx.fragment.app.Fragment;
 
 import static com.facebook.FacebookSdk.getApplicationContext;
 
-public class NotificationFragment extends Fragment {
+public class NotificationFragment extends Fragment{
 
     @Nullable
     @Override
@@ -28,6 +33,7 @@ public class NotificationFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_notification, container, false);
 
+        // TODO need to replace these hardcoded data
         final String names[] = {"Gina Kim","Nick McGrath","Yuni Hur"};
         final String schools[] = {"British Columbia Institute of Technology","British Columbia Institute of Technology","British Columbia Institute of Technology"};
         final int images[] = {R.drawable.logo, R.drawable.logo, R.drawable.logo};
@@ -76,12 +82,51 @@ public class NotificationFragment extends Fragment {
             TextView aName = row.findViewById(R.id.textView1);
             TextView aSchool = row.findViewById(R.id.textView2);
 
-
             imageView.setImageResource(rImages[position]);
             aName.setText(rNames[position]);
             aSchool.setText(rSchools[position]);
 
+            //Handle buttons and add onClickListeners
+            Button btnViewProfile = (Button)row.findViewById(R.id.btn_view_profile);
+            //Button btnAccept = (Button)row.findViewById(R.id.btn_accept);
+            //Button btnDecline = (Button)row.findViewById(R.id.btn_decline);
+
+            btnViewProfile.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    showDialog();
+                }
+            });
+
             return row;
+        }
+
+        public void showDialog(){
+            final Dialog dialog = new Dialog(getActivity());
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+            dialog.setContentView(R.layout.popup_profile);
+
+            ImageView image = dialog.findViewById(R.id.profile_image);
+            TextView name = dialog.findViewById(R.id.profile_name);
+            TextView school = dialog.findViewById(R.id.profile_school);
+            TextView major = dialog.findViewById(R.id.profile_major);
+
+            // TODO need to replace these hardcoded data with user info from database or google account
+            //image.setImageResource(rImages[0]);
+            name.setText(rNames[0]);
+            major.setText("Study "+"CST");
+            school.setText("at "+rSchools[0]);
+
+            Button btnClose = dialog.findViewById(R.id.btn_close);
+            btnClose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+
+            dialog.show();
         }
     }
 
